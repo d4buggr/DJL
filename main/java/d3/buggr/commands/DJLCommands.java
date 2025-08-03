89d5d0f01b83ad8c28b04ee2460942bfab457d0a
d3buggr <137682979+d4buggr@users.noreply.github.com>
2025-08-03 10:37:49 +0200

    Update DJLCommands.java

--- main/java/d3/buggr/commands/DJLCommands.java
+++ main/java/d3/buggr/commands/DJLCommands.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class TrustCommands extends ListenerAdapter implements TrustCommandsImpl {
+public class DJLCommands extends ListenerAdapter implements DJLCommandsImpl {
 
     @Override
     public String command() {
